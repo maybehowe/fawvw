ffbe65326c3385d85581ac1d712821bf4e737a2e
Kangning <liu.kangning@163.com>
2015-11-21 13:35:33 +0800

    loaidng page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 var oPage = {
 	pages: $('.page'),
+	images: $('img'),
 	init: function(){
 		var self = this
 
 		self.bind()
+		self.load()
+
+		$('#load_scratch').click(function(event) {
+			var oThis = $(this)	
+			oThis.removeClass('show')
+			self.show('4_1')
+		})
 	},
 	bind: function(){
 
@@ -13,10 +21,27 @@ var oPage = {
 
 		self.pages.hide()
 		$('#page_'+target).show()
+	},
+	load: function(){
+		var self = this,
+			iImgCount = self.images.length,
+			iImgTmp = 0
+
+		self.images.each(function(){
+			var oThis = $(this)
+			oThis.on('load', function(){
+				iImgTmp ++
+
+				if(iImgTmp == iImgCount){
+					$('#load_loading').removeClass('show')
+					$('#load_scratch').addClass('show')
+				}
+			})
+		})
 	}
 }
 
 $(function(){
 	oPage.init()
-	oPage.show('4_2')
+	// oPage.show('4_2')
 })
